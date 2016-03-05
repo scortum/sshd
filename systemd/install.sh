2dd5082c99f8e25e45fc8c411326d3609bb87598
Alex <alex@aeffle.de>
2016-03-05 22:07:14 +0100

    add systemd install script

---
+++ systemd/install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+
+IMAGE_NAME=scortum/sshd
+VERSION=latest
+DOCKER_CONTAINER_NAME=scortum-sshd
+
+
+cat > /etc/default/${DOCKER_CONTAINER_NAME} << EOF
+DOCKER_IMAGE=${IMAGE_NAME}:${VERSION}
+DOCKER_CONTAINER_NAME=${DOCKER_CONTAINER_NAME}
+LOCAL_DIR=/data/${DOCKER_CONTAINER_NAME}
+EOF
+
+
+cat > /lib/systemd/system/${DOCKER_CONTAINER_NAME}.service << EOF
+[Unit]
+Description=Some description here...
+Requires=docker.service
+After=docker.service
+
+[Service]
+Restart=on-failure
+RestartSec=10
+TimeoutStartSec=0
+EnvironmentFile=-/etc/default/${DOCKER_CONTAINER_NAME}
+ExecStartPre=-/usr/bin/docker kill \${DOCKER_CONTAINER_NAME}
+ExecStartPre=-/usr/bin/docker rm \${DOCKER_CONTAINER_NAME}
+ExecStartPre=/usr/bin/docker pull \${DOCKER_IMAGE}
+ExecStart=/usr/bin/docker run --name \${DOCKER_CONTAINER_NAME}      \
+                              -p 2222:22                            \
+                              -v \${LOCAL_DIR}:/data                \
+                              -v /etc/localtime:/etc/localtime:ro   \
+                              \${DOCKER_IMAGE}
+ExecStop=/usr/bin/docker stop --time=10 \${DOCKER_CONTAINER_NAME} 
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+
